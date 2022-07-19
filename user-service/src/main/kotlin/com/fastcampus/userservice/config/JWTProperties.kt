25040b7f422abeb9dd68580599debf422ade381f
sanghoon <digimon.1740@gmail.com>
2022-07-20 01:12:16 +0900

    JWT 기반 인증 구현

---
+++ user-service/src/main/kotlin/com/fastcampus/userservice/config/JWTProperties.kt
@@ -0,0 +1,13 @@
+package com.fastcampus.userservice.config
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "jwt")
+data class JWTProperties(
+    val issuer: String,
+    val subject: String,
+    val expiresTime: Long,
+    val secret: String,
+)
